3850e23a57d6f8e2acb92a425c65bb74624ac933
yuksuchi <yukauchiovo@gmail.com>
2025-11-04 21:11:01 +0800

    Updates

--- _next/static/CZ0GIXerjWIS9ODSP7uoY/_buildManifest.js
+++ _next/static/CZ0GIXerjWIS9ODSP7uoY/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Meng-Meng-Cha-wu-2//_next/:path+",
+        "destination": "/Meng-Meng-Cha-wu-2/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
